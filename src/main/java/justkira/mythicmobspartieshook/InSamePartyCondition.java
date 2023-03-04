ad406a14ae75f59198ed29fc9bf8ca23b9a32ce8
JustKira <shighgamerhg@gmail.com>
2023-03-04 13:49:46 +0300

    init

---
+++ src/main/java/justkira/mythicmobspartieshook/InSamePartyCondition.java
@@ -0,0 +1,44 @@
+package justkira.mythicmobspartieshook;
+
+import com.alessiodp.parties.api.Parties;
+import com.alessiodp.parties.api.interfaces.PartiesAPI;
+import com.alessiodp.parties.api.interfaces.Party;
+import com.alessiodp.parties.api.interfaces.PartyPlayer;
+import io.lumine.mythic.api.adapters.AbstractEntity;
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.api.skills.conditions.IEntityComparisonCondition;
+
+public class InSamePartyCondition implements IEntityComparisonCondition {
+
+    public InSamePartyCondition(MythicLineConfig config) {
+
+    }
+
+    @Override
+    public boolean check(AbstractEntity caster, AbstractEntity target) {
+
+        if (!target.isPlayer()) {
+            return false;
+        }
+        PartiesAPI api = Parties.getApi();
+        PartyPlayer casterPlayer = api.getPartyPlayer(caster.getUniqueId());
+        PartyPlayer targetPlayer = api.getPartyPlayer(target.getUniqueId());
+        if (!targetPlayer.isInParty()) {
+            return false;
+        }
+        if(targetPlayer.isInParty() && casterPlayer.isInParty()) {
+            Party casterPartyId = api.getParty(casterPlayer.getPartyId());
+            Party targetPartyId = api.getParty(targetPlayer.getPartyId());
+
+            if(casterPartyId == targetPartyId) {
+                return true;
+
+            }else
+            {
+                return false;
+            }
+        }
+        //PartiesApi api = Parties.getApi();
+        return false;
+    }
+}
---
+++ src/main/java/justkira/mythicmobspartieshook/MythicMobsPartiesHook.java
@@ -0,0 +1,50 @@
+package justkira.mythicmobspartieshook;
+
+import io.lumine.mythic.bukkit.events.MythicConditionLoadEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class MythicMobsPartiesHook extends JavaPlugin implements Listener {
+
+    private Logger log;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        log = this.getLogger();
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        log.info("MythicMobs API Examples Plugin Enabled!");
+
+        System.out.println("Plugin Started");
+        if (getServer().getPluginManager().getPlugin("Parties") != null) {
+            if (getServer().getPluginManager().getPlugin("Parties").isEnabled()) {
+                System.out.println("Parties Hooked");
+
+            }
+        }
+        if (getServer().getPluginManager().getPlugin("MythicMobs") != null) {
+            if (getServer().getPluginManager().getPlugin("MythicMobs").isEnabled()) {
+                System.out.println("MythicMobs Hooked");
+            }
+        }
+    }
+    @EventHandler
+    public void onMythicConditionLoad(MythicConditionLoadEvent event)	{
+        log.info("MythicConditionLoadEvent called for condition " + event.getConditionName());
+
+        if(event.getConditionName().equalsIgnoreCase("EXAMPLE"))	{
+            event.register(new InSamePartyCondition(event.getConfig()));
+            log.info("-- Registered condition!");
+        }
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
